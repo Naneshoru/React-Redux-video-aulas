f995f0c47ac654160576be3a6df658e219729653
Ricardo Atakiama <atakiama@usp.br>
2022-02-03 17:26:50 -0300

    Usando css grid areas para dividir os espaçcos da pagina

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -7,8 +7,26 @@ import './Video.css'
 const Video = ({ activeModule, activeLesson }) => {
   return ( 
     <div className='video-container'>
-      <strong>Modulo: {activeModule.title}</strong>
-      <span>/&nbsp;Aula: {activeLesson.title}</span>
+
+      <div className='video-container__video-header'>
+        <p>
+          <strong>Modulo: {activeModule.title}</strong>
+          <span>/&nbsp;Aula: {activeLesson.title}</span>
+        </p>
+      </div>
+
+      <div className='video-container__video-area'>
+        <video
+          controls
+          autoplay
+          preload='metadata'
+        >
+        </video>
+      </div>
+      <div className='video-container__video-side'></div>
+      <div className='video-container__video-footer '>
+        footer
+      </div>
     </div>
   );
 }
