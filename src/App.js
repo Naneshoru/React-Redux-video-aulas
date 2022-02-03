2c2bc258cc631e8eae01fb4c2b130e34b90e2b96
Ricardo Atakiama <atakiama@usp.br>
2022-02-03 14:38:42 -0300

    Utilizando o Provider no App para enviar os estados para os componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Video from './components/Video/Video';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+class App extends Component {
+
+  render () {
+    return (
+      <div className="App">
+          <Provider store={store}>
+            <Video />
+            <Sidebar />
+          </Provider>
+      </div>
+    )
+  } 
+  
 }
 
 export default App;
