7444074ee45e31fe89b64dc57efc079c9641cf60
Ricardo Atakiama <atakiama@usp.br>
2022-02-28 03:54:13 -0300

    Mostrando ícone do status da aula - visualized

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import './Sidebar.css';
+import CircleIcon from '@mui/icons-material/Circle';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const Sidebar = ({ modules, dispatch }) => {
 
@@ -12,6 +15,28 @@ const Sidebar = ({ modules, dispatch }) => {
     }
   }
 
+  let lessonsSize = 0;
+
+  useEffect(() => {
+    const getLessonsSize = () => {
+      modules.forEach((module) => {
+        module.lessons.forEach((lesson) => {
+          lessonsSize++;
+        })
+      })
+    }
+    getLessonsSize();
+  }, [modules, lessonsSize])
+
+  const [lessonsStatus, setLessonsStatus] = useState(Array(lessonsSize).fill(''));
+
+  const updateCheckedArray = (value, index) => {
+    let newArray = [...lessonsStatus];
+    newArray[index] = value;
+    setLessonsStatus(newArray);
+  }
+
+
   return ( 
     <div className='sidebar-container'>
       <aside>
@@ -19,13 +44,19 @@ const Sidebar = ({ modules, dispatch }) => {
           <div key={module.id}>
             <p><strong>{module.title}</strong></p>
             <ul className='sidebar__line'>
-              { module.lessons.map((lesson) => (
+              { module.lessons.map((lesson, i) => (
                 <li 
                   key={lesson.id} 
-                  onClick={() => dispatch(toggleLesson(module, lesson))}
+                  onClick={() => { 
+                    dispatch(toggleLesson(module, lesson)); 
+                    updateCheckedArray('visualized', i)
+                  }}
                   className='sidebar__line-item'
                 >
-                  {lesson.title}
+                  {!lessonsStatus[i] && <div><CircleIcon /></div>}
+                  {lessonsStatus[i] === 'visualized' && <div><VisibilityIcon /></div>}
+                  {lessonsStatus[i] === 'watched' && <div><CheckCircleIcon /></div>}
+                  <div><span>{lesson.title}</span></div>
                 </li>
               ))}
             </ul>
@@ -34,7 +65,6 @@ const Sidebar = ({ modules, dispatch }) => {
       </aside>
 
     </div>
-
   );
 }
  
