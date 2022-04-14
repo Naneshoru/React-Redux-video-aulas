8feab97ea7cb65b80d45a11d3902255f99ace0d4
Ricardo Atakiama <atakiama@usp.br>
2022-04-14 02:48:32 -0300

    Removendo import nãao usado. Reclamando no build netlify

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import './Sidebar.css';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
